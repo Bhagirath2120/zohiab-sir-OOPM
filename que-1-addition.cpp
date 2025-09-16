ea365b23793c87a73f9705aba22fbc115c6c6aa8
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-16 10:39:48 +0530

    default argument

---
+++ que-1-addition.cpp
@@ -0,0 +1,33 @@
+// add value using different data-type
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int add(int a,int b)
+{cout<<"int version"<<endl;
+ return a+b;
+}
+float add(float a,int b){
+  cout<<"float version 1 "<<endl;
+  return a+b;
+
+}
+float add(int a,float b){
+    cout<<"float version 2"<<endl;
+    return a+b;
+}
+double add(double a,double b){
+    cout<<"double version"<<endl;
+    return a+b;
+}
+
+
+int main(){
+
+//int a; float b;
+
+cout<<"Addition: "<< add(5.55f,5);
+
+    getch();
+}
