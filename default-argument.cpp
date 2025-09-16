fe6f68f635a2c2248f447b862ee1babe78cf1087
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-16 10:39:43 +0530

    default argument

---
+++ default-argument.cpp
@@ -0,0 +1,19 @@
+//Default argument
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int add(int a,int b,int c=0)
+{cout<<"int version"<<endl;
+ return a+b+c;
+}
+
+int main(){
+
+//int a; float b;
+
+cout<<"Addition: "<< add(5,5);
+
+    getch();
+}
